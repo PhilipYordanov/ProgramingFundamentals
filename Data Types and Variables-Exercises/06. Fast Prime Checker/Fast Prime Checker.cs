339b6403d3a07b525739c68b2a1bdf7efbeadd8a
PhilipYordanov <fjordanov1@gmail.com>
2017-04-14 08:27:39 +0300

    Fast Prime Checker.cs Different Integers Size.cs

---
+++ Data Types and Variables-Exercises/06. Fast Prime Checker/Fast Prime Checker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Fast_Prime_Checker
+{
+    public static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= inputNumber; i++)
+        {
+            bool isPrime = true;
+
+            for (int divider = 2; divider <= Math.Sqrt(i); divider++)
+            {
+                if (i % divider == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine($"{i} -> {isPrime}");
+        }
+    }
+}
+
